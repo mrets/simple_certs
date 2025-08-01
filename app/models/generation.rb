23b08991977faf11634a9beb8b3e4eca212dc376
Steve Tuckner <steve@mrets.org>
2025-07-31 21:50:44 -0500

    added validations for generation

--- app/models/generation.rb
+++ app/models/generation.rb
@@ -6,10 +6,36 @@ class Generation < ApplicationRecord
 
   after_create :issue_certificate
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :quantity, presence: true
+
+  validate :start_date_is_less_than_end_date
+  validate :start_date_is_not_in_the_future
+  validate :end_date_is_not_in_the_future
+
   def issue_certificate
     self.certificate = Certificate.new(
       quantity: self.quantity,
       generator: self.generator
     )
+  end
+
+  def start_date_is_less_than_end_date
+    if start_date > end_date
+      errors.add(:base, "start date #{start_date} is not less the than end date #{end_date}")
+    end
+  end
+
+  def start_date_is_not_in_the_future
+    if start_date > Date.today
+      errors.add(:base, "start date #{start_date} is in the future")
+    end
+  end
+
+  def end_date_is_not_in_the_future
+    if end_date > Date.today
+      errors.add(:base, "end date #{end_date} is in the future")
+    end
   end
 end
--- spec/models/generation_spec.rb
+++ spec/models/generation_spec.rb
@@ -1,10 +1,59 @@
 require 'rails_helper'
 
 describe Generation do
-  subject { create(:generation, quantity: 5, certificate: nil) }
+
+  context 'when testing start and end dates' do
+    subject { build(:generation, start_date: start_date, end_date: end_date) }
+
+    context 'when start date is before the end date' do
+      let(:start_date) { 2.days.ago }
+      let(:end_date) { 1.day.ago }
+
+      it 'be valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when start date is equal to the end date' do
+      let(:start_date) { 1.days.ago }
+      let(:end_date) { 1.day.ago }
+
+      it 'be valid' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'when start date is after the end date' do
+      let(:start_date) { 1.days.ago }
+      let(:end_date) { 2.day.ago }
+
+      it 'not be valid' do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'when the end date is in the future' do
+      let(:start_date) { 1.days.ago }
+      let(:end_date) { 1.day.from_now }
+
+      it 'not be valid' do
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'when the start date is in the future' do
+      let(:start_date) { 1.days.from_now }
+      let(:end_date) { 2.days.from_now }
+
+      it 'not be valid' do
+        expect(subject).not_to be_valid
+      end
+    end
+  end
 
   # issue_certificate is called after_create
   describe '#issue_certificate' do
+    subject { create(:generation, quantity: 5, certificate: nil) }
     let(:certificate) { subject.certificate }
 
     it 'creates a certificate with the same quantity as the generation' do
